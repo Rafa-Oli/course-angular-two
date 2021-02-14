888556fb8ef31902c8d4596536f1c753cd5ef6bb
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-02-14 20:21:13 -0300

    creating rating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { OrderItensComponent } from './order/order-itens/order-itens.component';
 import { OrderService } from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
+import { RatingComponent } from './shared/rating/rating.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
     RadioComponent,
     OrderItensComponent,
     DeliveryCostsComponent,
-    OrderSumaryComponent
+    OrderSumaryComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartItem } from 'app/restaurantes/restaurant-detail/shopping-cart/cart-item.model';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { Order, OrderItem } from './order.model';
@@ -18,7 +19,7 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão de Refeição', value: 'REF'}
   ]
 
-  constructor( private orderService: OrderService) { }
+  constructor( private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -43,8 +44,8 @@ export class OrderComponent implements OnInit {
   checkOrder(order: Order){
     order.orderItems = this.cartItems().map((item:CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order).subscribe((orderId: string) =>{
+      this.router.navigate(['/order-sumary'])
       this.orderService.clear()
-
     })
 
   }
