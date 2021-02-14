0ade7e82c8e2e22e9276a9fc3e9bde41cba558ec
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-02-14 19:26:20 -0300

    implementing the post order method

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from "@angular/core";
+import { Http, RequestOptions, Headers } from "@angular/http";
+import { MEAT_API } from "app/app.api";
 import { CartItem } from "app/restaurantes/restaurant-detail/shopping-cart/cart-item.model";
 import { ShoppingCartService } from "app/restaurantes/restaurant-detail/shopping-cart/shopping-cart.service";
+import { Observable } from "rxjs/Observable";
+import { Order } from "./order.model";
 
 @Injectable()
 
 export class OrderService{
-    constructor(private cartService: ShoppingCartService){}
+    constructor(private cartService: ShoppingCartService, private http: Http){}
 
     cartItems(): CartItem[]{
         return this.cartService.itens
@@ -21,5 +25,13 @@ export class OrderService{
     }
     itemsValue(): number{
         return this.cartService.total()
+    }
+    clear(){
+        this.cartService.clear();
+    }
+    checkOrder(order: Order): Observable<string>{
+        const headers= new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order),new RequestOptions({headers: headers})).map(response => response.json())
     }
 }
