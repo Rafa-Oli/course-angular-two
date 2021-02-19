27023fff64a64cafdb14e3dc9bee12e026a252df
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-02-18 21:18:22 -0300

    implementing keyframes for more complex animations

--- src/app/restaurantes/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurantes/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,34 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service';
-
+import { trigger, state, style, transition, animate, keyframes } from "@angular/animations";
 @Component({
   selector: 'mt-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css'],
+  animations: [
+    trigger('row', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', animate('300ms 0s ease-in', keyframes([
+        style({ opacity: 0, transform: 'translateX(-30px)', offset: 0 }),
+        style({ opacity: 0.8, transform: 'translateX(10px)', offset: 0.8 }),
+        style({ opacity: 1, transform: 'translateX(0px)', offset: 1 }),
+      ]))),
+      transition('ready => void', animate('300ms 0s ease-out', keyframes([
+        style({ opacity: 1, transform: 'translateX(0px)', offset: 0 }),
+        style({ opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2 }),
+        style({ opacity: 0, transform: 'translateX(30px)', offset: 1 }),
+      ])))
+    ])
+  ]
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent {
+  rowState = 'ready'
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
-  ngOnInit() {
-  }
-
-  items(): any[]{
+  public items(): any[] {
     return this.shoppingCartService.itens;
   }
-
-  total(): number{
+  public total(): number {
     return this.shoppingCartService.total()
   }
-  clear(){
+  public clear(): void {
     this.shoppingCartService.clear()
   }
-  removeItem(item:any){
+  public removeItem(item: any): void {
     this.shoppingCartService.removeItem(item)
   }
 
-  addItem(item: any){
+  public addItem(item: any): void {
     this.shoppingCartService.addItem(item)
   }
-
 }
