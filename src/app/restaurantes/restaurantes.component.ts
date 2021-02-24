b753e665918aaac57607c8e7c47934f3a171126b
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-02-23 21:00:40 -0300

    implementing search if item is not found

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -7,12 +7,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import 'rxjs/add/operator/switchMap'
 import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'mt-restaurantes',
   templateUrl: './restaurantes.component.html',
   styleUrls: ['./restaurantes.component.css'],
-  providers:[RestaurantsService],
+  providers: [RestaurantsService],
   animations: [
     trigger('toggleSearch', [
       state('hidden', style({
@@ -21,8 +24,8 @@ import 'rxjs/add/operator/distinctUntilChanged'
       })),
       state('visible', style({
         opacity: 1,
-      "max-height": "70px",
-      "margin-top": "20px"
+        "max-height": "70px",
+        "margin-top": "20px"
       })),
       transition('visible <=> hidden', animate('360ms os ease-in-out'))
     ])
@@ -44,14 +47,15 @@ export class RestaurantesComponent implements OnInit {
     this.searchForm.get('search').valueChanges
       .debounceTime(500) //vai mandar result depois de 500ms
       .distinctUntilChanged() //vai verificar se é igual ao ultimo result
-      .switchMap((value) => this.restaurantsService.restaurants(value))
+      .switchMap((value) => this.restaurantsService.restaurants(value)
+        .catch(error => Observable.from([]))) // se der erro na requisição, vai retorna um vazio, para não quebrar
       .subscribe(restaurants => this.restaurants = restaurants)
 
     this.restaurantsService.restaurants()
-    .subscribe(restaurants => this.restaurants = restaurants)
+      .subscribe(restaurants => this.restaurants = restaurants)
   }
 
-  public toggleSearch(): void{
+  public toggleSearch(): void {
     this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
   }
 }
